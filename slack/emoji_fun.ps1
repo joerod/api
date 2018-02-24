dde53778d87ab0f76d5bac6fd05f4ba1afffc0ad
Joe Rodriguez <joerod@gmail.com>
2018-02-24 13:29:04 -0500

    Create emoji_fun.ps1

---
+++ slack/emoji_fun.ps1
@@ -0,0 +1,24 @@
+#Emoji bomb your slack channel.  Add your slack webhook to the Send-SlackMessage function
+Function Send-SlackMessage {
+    Param(
+        [Parameter(Position = 0, mandatory = $true)]
+        [string]$Message,
+        $Webhook = ""
+    )
+$splat = @{'Uri'  = "https://hooks.slack.com/services/$webhook"
+    'Method'      = 'post'
+    'ContentType' = 'application/json' 
+    'Body'        = "{""text"":""$message"",}"
+}
+   $response =  Invoke-RestMethod @splat
+   if ($response -eq "ok"){$null}
+
+    }
+[int]$x = '1'   
+while ($x -le 50 ) {
+    $x++
+    $message = Invoke-RestMethod -Uri "https://raw.githubusercontent.com/github/gemoji/master/db/emoji.json" | Sort-Object {Get-Random} | Select-Object -Last 1
+    $final
+    $emoji_message = (":" + $final + ":")
+    Send-SlackMessage -Message "$emoji_message" -Webhook "here"
+}
